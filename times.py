994bad079bcf0cf4c2a40336be366d86449fd803
Nikitas Rigas Kalogeropoulos <kalogeropo@ceid.upatras.gr>
2023-08-29 15:23:15 +0300

    get timings
    Signed-off-by: Nikitas Rigas Kalogeropoulos <kalogeropo@ceid.upatras.gr>

--- times.py
+++ times.py
@@ -7,7 +7,20 @@ from models.WindowedGSB import WindowedGSBModel
 from models.borda_count import BordaCount
 
 for i in range(0,9):
+    print(f"====================iteration {i}======================================")
     path = 'collections/CF/docs'
+    path_to_write = 'data/test_docs/tests'
+    col_path = 'data'
+    testcol = Collection(path, name="test")
+    testcol.create_collection()
+    testcol.save_inverted_index(path_to_write)
+    q, r = testcol.load_collection(col_path)
+
+    N = GSBModel(testcol)
+
+for i in range(0,5):
+    print(f"====================iteration {i}======================================")
+    path = 'collections/CF_XL/docs'
     path_to_write = 'data/test_docs/tests'
     col_path = 'data'
     testcol = Collection(path, name="test")
