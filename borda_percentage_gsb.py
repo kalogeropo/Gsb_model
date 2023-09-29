e49b7c23aff66c998833ad7027acdb0304da1735
Nikitas Rigas Kalogeropoulos <kalogeropo@ceid.upatras.gr>
2023-09-29 19:46:42 +0300

    borda_testing_p1
    Signed-off-by: Nikitas Rigas Kalogeropoulos <kalogeropo@ceid.upatras.gr>

--- borda_percentage_gsb.py
+++ borda_percentage_gsb.py
@@ -1,27 +1,30 @@
+from numpy import mean
+from pandas import DataFrame
+
 from Preprocess.Collection import Collection
 from models.GSB import GSBModel
 from models.WindowedGSB import WindowedGSBModel
 from models.borda_count import BordaCount
-from utilities.document_utls import res_to_excel
+from utilities.Result_handling import res_to_excel, expir_start, write
 
 """percentage window testing """
 
-path = 'collections/CF/docs'
-path_to_write = 'data/test_docs/tests'
-col_path = 'data'
-testcol = Collection(path, name="test")
-testcol.create_collection()
-testcol.save_inverted_index(path_to_write)
-q, r = testcol.load_collection(col_path)
+path = 'experiments/collections/CF/docs'
+path_to_write = 'Gsb_model/data/test_docs/tests'
+col_path = 'experiments/collections/CF'
+dest_path = "experiments/paper_results"
+testcol, q, r = expir_start(path, path_to_write, col_path)
 
 list_of_prec = [i for i in range(0,100,5)]
-
+map_perc_wind =[]
+map_borda = []
 for i in list_of_prec:
     print(f"{list_of_prec.index(i)+1} of {len(list_of_prec)}")
     perc = i/100
     M = WindowedGSBModel(testcol,perc)
     M.fit(min_freq=10)
     M.evaluate()
+    map_perc_wind.append(mean(M.precision))
 
     N = GSBModel(testcol)
     N.fit(min_freq=10)
@@ -30,5 +33,9 @@ for i in list_of_prec:
     bord = BordaCount(M.ranking, N.ranking, testcol)
     bord.fit()
     bord.evaluate()
-    dest_path= "collections/test/Results"
-    res_to_excel(bord,"testBord_percentage_Gsb.xlsx",dest_path,sheetname=f"gsb_perc_{i}")
+    map_borda.append(mean(bord.precision))
+
+    #res_to_excel(bord,"testBord_percentage_Gsb.xlsx",dest_path,sheetname=f"gsb_perc_{i}")
+
+df = DataFrame(list(zip(map_perc_wind,map_borda)), columns=["Perc","Borda"])
+write(xl_namefile="testBord_percentage_Gsb.xlsx", dest_path=dest_path, sheetname="acc_borda_perc_gsb", data=df)
--- borda_win7_set_apriori1.py
+++ borda_win7_set_apriori1.py
@@ -1,18 +1,22 @@
 from networkx import info
+from numpy import mean
+from pandas import DataFrame
 
 from models.SetBased import SetBasedModel
-from utilities.document_utls import res_to_excel
+from utilities.Result_handling import expir_start, write, res_to_excel
 from Preprocess.Collection import Collection
 from models.WindowedGSB import WindowedGSBModel
 from models.borda_count import BordaCount
 
-path = 'collections/CF/docs'
-path_to_write = 'data/test_docs/tests'
-col_path = 'data'
-testcol = Collection(path, name="test")
-testcol.create_collection()
-testcol.save_inverted_index(path_to_write)
-q, r = testcol.load_collection(col_path)
+path = 'experiments/collections/CF/docs'
+path_to_write = 'Gsb_model/data/test_docs/tests'
+col_path = 'experiments/collections/CF'
+dest_path = "experiments/paper_results"
+testcol, q, r = expir_start(path, path_to_write, col_path)
+
+map_borda = []
+map_wind7 = []
+map_set = []
 support = [2, 3, 4, 5, 10, 15,20]
 for sup in support:
     print(f"{support.index(sup) + 1} out of {len(support)}")
@@ -20,14 +24,20 @@ for sup in support:
     M = WindowedGSBModel(testcol,7)
     M.fit(min_freq=sup)
     M.evaluate()
+    map_wind7.append(mean(M.precision))
 
     N = SetBasedModel(testcol)
     N.fit(min_freq=sup)
     N.evaluate()
+    map_set.append(mean(N.precision))
 
     bord = BordaCount(M.ranking, N.ranking, testcol)
     bord.fit()
     bord.evaluate()
-    dest_path= "collections/test/Results"
+    map_borda.append(mean(bord.precision))
+
     res_to_excel(bord,"borda_set_wind7_apriori.xlsx",dest_path,sheetname=f"set_wind7_apri_{sup}")
+
+df = DataFrame(list(zip(map_set,map_wind7,map_borda)), columns=["set","win7","Borda"])
+write(xl_namefile="borda_set_wind7_apriori.xlsx", dest_path=dest_path, sheetname="acc_borda_perc_gsb", data=df)
 
