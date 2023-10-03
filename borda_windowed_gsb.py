b271b53c47ca82618bb69308a6f6df90d7efdfc7
Nikitas Rigas Kalogeropoulos <kalogeropo@ceid.upatras.gr>
2023-10-03 17:40:05 +0300

    borda_testing_p4
    Signed-off-by: Nikitas Rigas Kalogeropoulos <kalogeropo@ceid.upatras.gr>

--- borda_windowed_gsb.py
+++ borda_windowed_gsb.py
@@ -1,36 +1,44 @@
+from numpy import mean
+from pandas import DataFrame
+
 from models.GSB import GSBModel
-from utilities.document_utls import res_to_excel
-from Preprocess.Collection import Collection
 from models.WindowedGSB import WindowedGSBModel
 from models.borda_count import BordaCount
+from utilities.Result_handling import expir_start, res_to_excel, write
 
 '''Borda count  window GSB model testing '''
+path = 'experiments/collections/CF/docs'
+path_to_write = 'Gsb_model/data/test_docs/tests'
+col_path = 'experiments/collections/CF'
+dest_path = "experiments/paper_results"
+testcol, q, r = expir_start(path, path_to_write, col_path)
 
-path = 'collections/CF/docs'
-path_to_write = '/data/test_docs/tests'
-col_path = 'data'
-testcol = Collection(path, name="test")
-testcol.create_collection()
-testcol.save_inverted_index(path_to_write)
-q, r = testcol.load_collection(col_path)
+map_borda = []
+map_wind = []
+map_gsb = []
 
 for i in range(5,26):
     print(i)
     M = WindowedGSBModel(testcol,i)
     M.fit(min_freq=10)
     M.evaluate()
+    map_wind.append(mean(M.precision))
 
     N = GSBModel(testcol)
     N.fit(min_freq=10)
     N.evaluate()
+    map_gsb.append(mean(N.precision))
 
     bord = BordaCount(M.ranking, N.ranking, testcol)
     bord.fit()
     bord.evaluate()
-    dest_path= "collections/test/Results"
+    map_borda.append(mean(bord.precision))
+
     #M
     #res_to_excel(M,"testM.xlsx",dest_path,sheetname="test13")
     #N
     #res_to_excel(N,"testN.xlsx",dest_path,sheetname="test16")
     #bord
     res_to_excel(bord,"Bord_wind_gsb.xlsx",dest_path,sheetname=f"gsb_win_{i}")
+df = DataFrame(list(zip(map_gsb,map_wind,map_borda)), columns=["GSB","WIND","Borda"])
+write(xl_namefile="Bord_wind_gsb.xlsx", dest_path=dest_path, sheetname="acc_borda_win_gsb", data=df)
--- borda_windowed_set.py
+++ borda_windowed_set.py
@@ -1,33 +1,39 @@
-from networkx import info
+from numpy import mean
+from pandas import DataFrame
 
 from models.SetBased import SetBasedModel
-from utilities.document_utls import res_to_excel
-from Preprocess.Collection import Collection
 from models.WindowedGSB import WindowedGSBModel
 from models.borda_count import BordaCount
+from utilities.Result_handling import res_to_excel, expir_start, write
 
-path = 'collections/CF/docs'
-path_to_write = 'data/test_docs/tests'
-col_path = 'data'
-testcol = Collection(path, name="test")
-testcol.create_collection()
-testcol.save_inverted_index(path_to_write)
-q, r = testcol.load_collection(col_path)
+path = 'experiments/collections/CF/docs'
+path_to_write = 'Gsb_model/data/test_docs/tests'
+col_path = 'experiments/collections/CF'
+dest_path = "experiments/paper_results"
+testcol, q, r = expir_start(path, path_to_write, col_path)
 
-# wind_list = [8,9,11,12,14,15,17,18,20,21,22,23,24,25]
-# for i in wind_list:
-#     print(f"{wind_list.index(i)} of {len(wind_list)}")
-#     M = WindowedGSBModel(testcol,i)
-#     M.fit(min_freq=10)
-#     M.evaluate()
-#
-#     N = SetBasedModel(testcol)
-#     N.fit(min_freq=10)
-#     N.evaluate()
-#
-#     bord = BordaCount(M.ranking, N.ranking, testcol)
-#     bord.fit()
-#     bord.evaluate()
-#     dest_path= "collections/test/Results"
-#     res_to_excel(bord,"testBord_set_win.xlsx",dest_path,sheetname=f"set_wind_{i}")
+wind_list = [8,9,11,12,14,15,17,18,20,21,22,23,24,25]
 
+map_borda = []
+map_wind = []
+map_set = []
+
+for i in wind_list:
+    print(f"{wind_list.index(i)} of {len(wind_list)}")
+    M = WindowedGSBModel(testcol,i)
+    M.fit(min_freq=10)
+    M.evaluate()
+    map_wind.append(mean(M.precision))
+
+    N = SetBasedModel(testcol)
+    N.fit(min_freq=10)
+    N.evaluate()
+    map_set.append(mean(N.precision))
+
+    bord = BordaCount(M.ranking, N.ranking, testcol)
+    bord.fit()
+    bord.evaluate()
+    map_borda.append(mean(bord.precision))
+    res_to_excel(bord,"testBord_set_win.xlsx",dest_path,sheetname=f"set_wind_{i}")
+df = DataFrame(list(zip(map_set,map_wind,map_borda)), columns=["Set","WIND","Borda"])
+write(xl_namefile="testBord_set_win.xlsx", dest_path=dest_path, sheetname="acc_borda_win_set", data=df)
